38e0c02e900e6885c11f4bb286f35059c0a7f59e
Ali Abbasi <ali_sitrick@yahoo.com>
2024-01-26 14:31:24 -0800

    added logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import Nav from "./Nav";
 import { useState } from "react";
+import logo from "../naj-movie-logo.svg";
 
 function Header() {
   const [showNav, setShowNav] = useState(false);
@@ -7,7 +8,7 @@ function Header() {
   return (
     <header className={`header ${showNav ? "show" : ""}`}>
       <div className="header-container">
-        
+        <img src={logo} alt="Naj Movie Logo" />
         <button
           onClick={() => {
             setShowNav(!showNav);
