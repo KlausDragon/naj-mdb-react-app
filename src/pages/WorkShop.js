fa19a38545fc0906dbc6984468a536ea4b53da5a
Nicole Sedillo <ntsedillo@live.ca>
2024-01-26 14:33:13 -0800

    modified MovieCards components

--- src/pages/WorkShop.js
+++ src/pages/WorkShop.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import MovieCard from "../components/MovieCard";
+import '../scss/_workshop.scss'
 
 function WorkShop() {
   const [upcomingMovies, setUpcomingMovies] = useState([]);
@@ -6,8 +8,14 @@ function WorkShop() {
   return (
     <div>
       <h2>Workshop</h2>
+      <div className="card">
+      <MovieCard/>
+      </div>
     </div>
   );
 }
 
+
 export default WorkShop;
+
+
