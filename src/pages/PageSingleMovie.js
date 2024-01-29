c034db99fb26db8b3c5d4e6d1cf5a2740ffd617f
shiangchinlin <150076701+shiangchinlin@users.noreply.github.com>
2024-01-28 19:09:42 -0800

    update banner and single page

--- src/pages/PageSingleMovie.js
+++ src/pages/PageSingleMovie.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { getMovieById } from "../utilities/api";
 import { useEffect } from "react";
 import { filterVideos, formatReleaseDate } from "../utilities/toolbelt";
+import { IMAGE_URL_BASE } from "../utilities/api";
 import FavouriteButton from "../components/FavoriteButton";
 
 function PageSingleMovie() {
@@ -16,7 +17,7 @@ function PageSingleMovie() {
       .then((data) => {
         console.log("Data", data);
 
-        console.log("safs", data.videos.results);
+        // console.log("safs", data.videos.results);
         const youtubeTrailerVideos = filterVideos(data.videos.results);
         setMovieData(data);
         setMovieVideos(youtubeTrailerVideos);
@@ -25,30 +26,51 @@ function PageSingleMovie() {
         alert(error);
       });
   }, []);
-  console.log("movieData", movieData);
-  console.log("movieVideos", movieVideos);
+  // console.log("movieData", movieData);
+  // console.log("movieVideos", movieVideos);
 
   return (
-    <div className="movie-page">
+    <div className="single-page">
       {movieData && (
-        <>
-          <h1>{movieData.title}</h1>
+        <div>
           <div>
-            <h2>{formatReleaseDate(movieData.release_date)}</h2>
-            <FavouriteButton movieData={movieData} />
+            <h1 className="single-title">{movieData.title}</h1>
+          </div>
+          <div>
+            <h2 className="single-date">{formatReleaseDate(movieData.release_date)}</h2>
+          </div>
 
-            <p>{movieData.overview}</p>
+          <div className="single-page-rating">
+            <p className="single-rating-text">{movieData.vote_average ? ( <p>{movieData.vote_average.toFixed(1).replace(".", "")}%</p> ) : ( <p>NR</p> )}</p>
+          </div>
 
-            <div className="movie-videos">
+            <div className="single-page-trailer">
               <iframe
                 src={`https://www.youtube.com/embed/${movieVideos[0].key}`}
-                width="600"
-                height="600"
+                // width="300"
+                // height="200"
                 title={movieData.name}
+                className="single-trailer"
               ></iframe>
             </div>
+
+          <div>
+              <img
+                className="single-page-poster"
+                src={`${IMAGE_URL_BASE}/w200/${movieData.poster_path}`}
+                alt={`${movieData.title}`}
+              />
           </div>
-        </>
+
+          <div>
+              <p>{movieData.overview}</p>
+          </div>
+
+          <div>
+          <FavouriteButton movieData={movieData} />
+          </div>
+
+        </div>
       )}
     </div>
   );
