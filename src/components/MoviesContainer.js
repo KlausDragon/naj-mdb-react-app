9e3bbbb42d347a13bd4eee40e77fb5d1b9bc35e2
Nicole Sedillo <ntsedillo@live.ca>
2024-02-05 23:30:28 -0800

    added a favorites page and made some changes to movie card and container

--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 function MoviesContainer({ title, moviesData }) {
   return (
     <div className="movies-container">
-      <h2>{title}</h2>
+      <h2 className="movies-container-title">{title}</h2>
       {moviesData.length > 0 &&
         moviesData.map((movieData) => {
           return <MovieCard key={movieData.id} movieData={movieData} />;
--- src/pages/PageFavorite.js
+++ src/pages/PageFavorite.js
@@ -1,9 +1,28 @@
-function Favorite() {
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalContext';
+import MovieCard from '../components/MovieCard'; 
+
+
+function FavoritesPage() {
+  const { favorites } = useContext(GlobalContext);
+
   return (
     <div>
-      <h1>Favorite</h1>
+      <h2 className="favorites-title">Your Favorites</h2>
+      
+      <div className="movies-container">
+        {favorites.length > 0 ? (
+          <div>
+            {favorites.map((movie) => (
+              <MovieCard key={movie.id} movieData={movie} />
+            ))}
+          </div>
+        ) : (
+          <p>No favorites yet. Add some movies to your favorites!</p>
+        )}
+      </div>
     </div>
   );
 }
 
-export default Favorite;
+export default FavoritesPage;
