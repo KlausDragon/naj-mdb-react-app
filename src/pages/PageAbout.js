c3f8fab0ff32eccbce814d555aa11717d5a4c1e8
Ali Abbasi <ali_sitrick@yahoo.com>
2024-02-09 13:32:06 -0800

    fixed about us styling!

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -5,22 +5,31 @@ function About() {
     <div className="about">
       <h1>Let's talk about YellowDoor</h1>
       <p>
-        Step to YellowDoor, where a simple tap opens the door to a mesmerizing
-        realm of cinematic wonders. Unleash the magic of movies at your
-        fingertips, as this application illuminates your path to a vast universe
-        of film worlds, trailers, reviews, and a ticket to unforgettable
-        entertainment. Explore the boundless possibilities of storytelling and
-        immerse yourself in the captivating glow of YellowDoor, where every tap
-        unfolds a new chapter in your cinematic journey. Welcome to the doorway
-        of cinematic delight!
+        Welcome to <span>Yellow</span>
+        <b>Door</b>, your premier gateway to the cinematic universe right at
+        your fingertips. As a distinguished portal, we offer an unparalleled
+        experience in discovering, exploring, and engaging with the world of
+        movies. Designed to seamlessly guide you through an expansive collection
+        of films, detailed reviews, captivating trailers, and easy ticket
+        purchasing, our platform ensures an unrivaled entertainment journey.
+        Passionate about movies and the powerful storytelling they offer,
+        YellowDoor aims to connect movie enthusiasts with the films they love
+        and introduce them to new cinematic treasures. Whether you're in search
+        of the latest blockbusters, timeless classics, or hidden indie gems, our
+        curated selection is bound to satisfy your cinematic cravings. Our
+        user-friendly interface makes every interaction simple yet fulfilling,
+        opening the door to a world where movies come alive. Dive into our vast
+        universe of film content, where each click brings you closer to the
+        stories that move, inspire, and entertain, all from the comfort of your
+        home or on the go. Welcome to the ultimate destination for film lovers,
+        where your next cinematic adventure begins with just a tap.
       </p>
-
+      <br />
       <p>
         This product uses the TMDB API but is not endorsed or certified by TMDB.
       </p>
 
       <img src={tmdbLogo} alt="TMDB Logo" />
-
     </div>
   );
 }
